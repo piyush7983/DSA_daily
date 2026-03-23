f2f212c1d3c90ffdd6eacf97c9645cbf78c98cc7
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2026-03-23 21:44:38 +0530

    0122-BEST-TIME-TO-BUY-AND-SELL-STOCK-II Stats: Time: 0 ms (100%), Space: 46.5 MB (37.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,17 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min=0;
+        int buy=0;
         int ans=0;
+        int sell=0;
         for(int i=1;i<prices.length;i++){
-            if(prices[i]>=prices[i-1]){
-                ans+=prices[i]-prices[i-1];
-                
+            if(prices[i-1]<=prices[i]){
+                sell=i;
             }
-            
-            
-                
-                
+            else{
+                ans+=prices[sell]-prices[buy];
+                buy=i;
+                sell=i;
+            }
         }
+        ans+=prices[sell]-prices[buy];
         return ans;
     }
 }
