f520478ac4671f300e3d95e50c2074d8d0f0c157
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2026-09-13 08:06:24 +0530

    0130-SURROUNDED-REGIONS Stats: Time: 15 ms (6.48%), Space: 47.6 MB (74.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -25,7 +25,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 
-                if(board[i][j]=='O' && !vis[i][j]){
+                if(!vis[i][j]  ){
                     board[i][j]='X';
                 }
             }
